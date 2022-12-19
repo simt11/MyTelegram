32eaeb1f425cbdf4db80070bb7bfe8a4cacca1dc
simt11 <simt11@yandex.ru>
2022-12-19 17:14:41 +0300

    Lesson 6
    1) Добавлено доп.меню в окне Настройки, с пунктами:
      а) Изменить имя
      б) Выбрать фотографию или видео
      в) Выход

--- app/src/main/java/com/example/mytelegram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/mytelegram/ui/fragments/SettingsFragment.kt
@@ -1,5 +1,7 @@
 package com.example.mytelegram.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import com.example.vovatelegram.R
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
@@ -7,6 +9,12 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
     // Методы необходимые для данного обьекта
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu,menu)
+    }
+
 
 }
