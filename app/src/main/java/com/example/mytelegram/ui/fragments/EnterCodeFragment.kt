0d802e73446c5334af7c5ab20dcc308409776289
simt11 <simt11@yandex.ru>
2022-12-20 17:21:12 +0300

    Lesson 7
    1) Добавлены два фрагмента Регистрации. Ввод телефона и код подтверждения.

---
+++ app/src/main/java/com/example/mytelegram/ui/fragments/EnterCodeFragment.kt
@@ -0,0 +1,8 @@
+package com.example.mytelegram.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.vovatelegram.R
+
+class EnterCodeFragment : Fragment(R.layout.fragment_enter_code) {
+
+}
---
+++ app/src/main/java/com/example/mytelegram/ui/fragments/EnterPhoneNumberFragment.kt
@@ -0,0 +1,8 @@
+package com.example.mytelegram.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.vovatelegram.R
+
+class EnterPhoneNumberFragment : Fragment(R.layout.fragment_enter_phone_number) {
+
+}
