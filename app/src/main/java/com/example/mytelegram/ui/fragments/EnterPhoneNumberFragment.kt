5dfd9b1c66fe7aae98612d977b53e4aa6a106c59
simt11 <simt11@yandex.ru>
2022-12-22 15:31:14 +0300

    Lesson_7
    Начата лекция

--- app/src/main/java/com/example/mytelegram/ui/fragments/EnterPhoneNumberFragment.kt
+++ app/src/main/java/com/example/mytelegram/ui/fragments/EnterPhoneNumberFragment.kt
@@ -1,8 +1,15 @@
 package com.example.mytelegram.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.vovatelegram.R
 
 class EnterPhoneNumberFragment : Fragment(R.layout.fragment_enter_phone_number) {
-
+    override fun onStart() {
+        super.onStart()
+        regi
+    }
 }
