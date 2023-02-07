a385d07dcaf4687248c6e2338ad2c1271ea1af4c
simt11 <simt11@yandex.ru>
2023-02-07 16:26:29 +0300

    Мелкие изменения

--- app/src/main/java/com/example/mytelegram/ui/fragments/EnterPhoneNumberFragment.kt
+++ app/src/main/java/com/example/mytelegram/ui/fragments/EnterPhoneNumberFragment.kt
@@ -45,6 +45,11 @@ class EnterPhoneNumberFragment : BaseFragment(R.layout.fragment_enter_phone_numb
             }
         }
         register_btn_next.setOnClickListener() { sendCode() }
+
+    }
+
+    override fun onStop() {
+        super.onStop()
     }
 
     private fun sendCode() {
